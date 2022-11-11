91b2435c22ca4362f0a2d50f45b341bfc90a3fa9
sakamoto <sakamoto051eng@gmail.com>
2022-11-11 18:34:54 +0000

    create user model

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\dummyAPI;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,3 +21,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::get('/data', [dummyAPI::class, 'getData']);
+
+Route::resource('user', UserController::class);
