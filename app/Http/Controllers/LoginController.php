610c47b92cbb14857177534aca797a87f51420c2
sakamoto051 <sakamoto051eng@gmail.com>
2022-11-14 13:47:52 +0000

    feat: sanctum auth

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -2,22 +2,35 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Foundation\Auth\User;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Auth;
 use Psy\Readline\Hoa\Exception;
-use Throwable;
 
 class LoginController extends Controller
 {
-    public function login(Request $request)
+    public function login(Request $request): JsonResponse
     {
-        $email = $request['email'];
-        try {
-            $user = User::where('email', '=', $email)->first();
-            return $user;
-        } catch (Throwable $e) {
-            throw new Exception($e);
+        $credentials = $request->validate([
+            'email' => ['required', 'email'],
+            'password' => 'required',
+        ]);
+
+        if (Auth::attempt($credentials)) {
+            $request->session()->regenerate();
+            return response()->json(['name' => Auth::user()->email], 200);
         }
+
+        throw new Exception('ログインに失敗しました。再度お試しください');
+    }
+
+    public function logout(Request $request): JsonResponse
+    {
+        Auth::guard('sanctum')->user()->tokens()->delete();
+        $request->session()->invalidate();
+        // $request->session()->regenerateToken();
+        $res = ['message' => 'logout!'];
+        return response()->json($res, Response::HTTP_OK);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -6,22 +6,13 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\dummyAPI;
 use App\Http\Controllers\UserController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+    Route::get('/logout', [LoginController::class, 'logout']);
 });
 
 Route::get('/data', [dummyAPI::class, 'getData']);
 
-Route::resource('user', UserController::class);
-Route::post('/login', [LoginController::class, 'login']);
+// Route::resource('user', UserController::class);
