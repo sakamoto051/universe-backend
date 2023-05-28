53d9be453ccc52ac59882b67591b77e99550440d
sakamoto051 <sakamoto051eng@gmail.com>
2023-05-28 01:28:12 +0000

    feat: redis development

--- config/database.php
+++ config/database.php
@@ -93,6 +93,14 @@ return [
             // 'trust_server_certificate' => env('DB_TRUST_SERVER_CERTIFICATE', 'false'),
         ],
 
+        'redis' => [
+            'client' => 'predis',
+            'host' => env('REDIS_HOST', '127.0.0.1'),
+            'password' => env('REDIS_PASSWORD', null),
+            'port' => env('REDIS_PORT', 6379),
+            'database' => 0,
+            'read_write_timeout' => 60,
+        ],
     ],
 
     /*
@@ -147,5 +155,22 @@ return [
         ],
 
     ],
+    // 'redis' => [
+    //     'client' => 'predis',
+    //     'options' => [
+    //         'cluster' => 'redis',
+    //     ],
+    //     'clusters' => [
+    //         'default' => [
+    //             [
+    //                 'host' => env('REDIS_HOST', '127.0.0.1'),
+    //                 'password' => env('REDIS_PASSWORD', null),
+    //                 'port' => env('REDIS_PORT', 6379),
+    //                 'database' => 0,
+    //                 'read_write_timeout' => env('REDIS_READ_WRITE_TIMEOUT', 60),
+    //             ]
+    //         ],
+    //     ],
+    // ],
 
 ];
