acae5cff5d175d70e8f526344cc80598da2fd8ef
sakamoto051 <sakamoto051eng@gmail.com>
2023-05-28 11:10:40 +0000

    feat: log

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -6,17 +6,20 @@ use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log;
 use Psy\Readline\Hoa\Exception;
 
 class LoginController extends Controller
 {
     public function login(Request $request): JsonResponse
     {
+        Log::debug($request);
         $credentials = $request->validate([
             'email' => ['required', 'email'],
             'password' => 'required',
         ]);
 
+        Log::debug(implode(',', $credentials));
         if (Auth::attempt($credentials)) {
             $request->session()->regenerate();
             return response()->json(['name' => Auth::user()->email], 200);
